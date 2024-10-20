cd5940a7abb59e9bd31fb58efcf01f65f8b3d5b9
VinodNimbalkar <omvinod@yahoo.com>
2024-10-20 18:06:44 +0530

    neovim config replaced with lazyvim

---
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,9 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin", opts = { transparent_background = true }, priority = 1000 },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
