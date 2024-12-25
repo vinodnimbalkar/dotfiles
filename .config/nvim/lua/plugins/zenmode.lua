096a028aa081534c1dde17f9b54fdb3ab45cbec1
VinodNimbalkar <omvinod@yahoo.com>
2024-12-25 14:56:28 +0530

    :sparkles: Add zenmode configuration with twilight

---
+++ .config/nvim/lua/plugins/zenmode.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      window = {
+        width = 120,
+      },
+      plugins = {
+        options = {
+          laststatus = 0,
+        },
+        twilight = {
+          enabled = false,
+        },
+      },
+    },
+  },
+}
